8f184556bfd82bd18e26c989da1c7f4f852f567d
Gustavo Souza <78645956+gustavelson@users.noreply.github.com>
2021-09-24 11:40:08 -0300

    Add files via upload

---
+++ projetocanvas/script.js
@@ -0,0 +1,77 @@
+// Initial Data
+
+let currentColor = 'black';
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
+
+let screen = document.querySelector('#tela');
+let save = document.querySelector('.save')
+let ctx = screen.getContext('2d');
+
+// Events
+
+document.querySelectorAll('.colorArea .color').forEach(item => {
+    item.addEventListener('click', colorClickEvent);
+});
+screen.addEventListener('mousedown', mouseDownEvent);
+screen.addEventListener('mousemove', mouseMoveEvent);
+screen.addEventListener('mouseup', mouseUpEvent);
+document.querySelector('.clear').addEventListener('click', clearScreen)
+document.querySelector('.save').addEventListener('click', saveScreen)
+
+// Functions
+
+function colorClickEvent(e) {
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+
+    document.querySelector('.color.active').classList.remove('active');
+    e.target.classList.add('active');
+}
+function mouseDownEvent(e) {
+    canDraw = true;
+    mouseX = e.pageX - screen.offsetLeft;
+    mouseY = e.pageY - screen.offsetTop;
+}
+function mouseMoveEvent(e) {
+    if(canDraw) {
+       draw(e.pageX, e.pageY);
+
+
+    }
+}
+function mouseUpEvent() {
+    canDraw = false;
+}
+function draw(x, y) {
+    let pointX = x - screen.offsetLeft;
+    let pointY = y - screen.offsetTop;
+
+    //Drawing
+
+    ctx.beginPath();
+    ctx.lineWidth = 5;
+    ctx.lineJoin = "round";
+    ctx.moveTo(mouseX, mouseY);
+    ctx.lineTo(pointX, pointY);
+    ctx.closePath();
+    ctx.strokeStyle = currentColor;
+    ctx.stroke();
+
+    mouseX = pointX;
+    mouseY = pointY;
+}
+function clearScreen() {
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+function saveScreen() {
+    let a = document.createElement("a");
+
+    document.body.appendChild(a);
+    a.href = screen.toDataURL();
+    a.download = "canvas-image.png";
+    a.click();
+    document.body.removeChild(a);
+}
